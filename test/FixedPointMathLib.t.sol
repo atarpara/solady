439469bccd091b4906104c313ed7aa56171db1af
atarpara <akpatel0618@gmail.com>
2025-12-29 18:30:23 +0530

    Check

--- test/FixedPointMathLib.t.sol
+++ test/FixedPointMathLib.t.sol
@@ -926,6 +926,7 @@ contract FixedPointMathLibTest is SoladyTest {
         assertEq(FixedPointMathLib.log2(2), 1);
         assertEq(FixedPointMathLib.log2(4), 2);
         assertEq(FixedPointMathLib.log2(10), 3);
+        assertEq(FixedPointMathLib.log2(20), 4);
         assertEq(FixedPointMathLib.log2(1024), 10);
         assertEq(FixedPointMathLib.log2(1048576), 20);
         assertEq(FixedPointMathLib.log2(1073741824), 30);
